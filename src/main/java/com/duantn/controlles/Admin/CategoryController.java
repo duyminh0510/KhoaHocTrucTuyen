a9167b8952e4e71a189117af21fb269f52f793d9
hoanggmagic <dinh22hoang2005@gmail.com>
2025-06-19 02:54:43 +0700

    feAdmin

---
+++ src/main/java/com/duantn/controlles/Admin/CategoryController.java
@@ -0,0 +1,14 @@
+package com.duantn.controlles.Admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CategoryController {
+
+    @GetMapping("/admin/category")
+    public String adminIndex() {
+        return "views/Admin/DanhMuc"; // -> sẽ tìm file:
+                                      // src/main/resources/templates/views/Admin/DanhMuc.html
+    }
+}
