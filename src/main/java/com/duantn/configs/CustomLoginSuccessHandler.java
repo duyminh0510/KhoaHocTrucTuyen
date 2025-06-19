80c0207d7ec88a7c210d741d4ff87e3fa7dafa4b
duyminh0510 <mduy2186@gmail.com>
2025-06-19 09:22:39 +0700

    daycodex6

--- src/main/java/com/duantn/configs/CustomLoginSuccessHandler.java
+++ src/main/java/com/duantn/configs/CustomLoginSuccessHandler.java
@@ -3,10 +3,14 @@ package com.duantn.configs;
 import java.io.IOException;
 import java.util.Collection;
 
+import org.springframework.security.web.savedrequest.SavedRequest;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.DefaultRedirectStrategy;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,11 +19,22 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
 
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
             HttpServletResponse response,
             Authentication authentication)
             throws IOException, ServletException {
+
+        SavedRequest savedRequest = new HttpSessionRequestCache().getRequest(request, response);
+
+        if (savedRequest != null) {
+            // Redirect to the originally requested URL (e.g. /gio-hang)
+            String targetUrl = savedRequest.getRedirectUrl();
+            redirectStrategy.sendRedirect(request, response, targetUrl);
+            return;
+        }
 
         Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
 
