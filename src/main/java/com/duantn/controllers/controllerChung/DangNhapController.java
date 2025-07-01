3a47dc830562c87a6afc05c9ef37619438f65e6c
Tran-Ngoc-Thai <ngocthaitran2k5@gmail.com>
2025-07-01 12:48:31 +0700

    Uppppp

--- src/main/java/com/duantn/controllers/controllerChung/DangNhapController.java
+++ src/main/java/com/duantn/controllers/controllerChung/DangNhapController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/auth")
 public class DangNhapController {
 
     @GetMapping("/login")
